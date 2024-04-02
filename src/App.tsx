6de4a692e20fa870747adeda6826b1cdd088356e
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-04-02 14:58:40 -0300

    css

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,11 @@ const urlsDef: links[] = [
   },
   {
     url: "https://www.amazon.com",
-    background: "",
+    background: "#febd69",
   },
   {
     url: "https://www.twitch.tv",
-    background: "",
+    background: "#6441a5",
   },
   {
     url: "https://discord.com",
@@ -30,7 +30,47 @@ const urlsDef: links[] = [
   },
   {
     url: "https://weather.com",
-    background: "",
+    background: "#005986",
+  },
+  {
+    url: "https://www.youtube.com",
+    background: "#FF0000",
+  },
+  {
+    url: "https://www.amazon.com",
+    background: "#febd69",
+  },
+  {
+    url: "https://www.twitch.tv",
+    background: "#6441a5",
+  },
+  {
+    url: "https://discord.com",
+    background: "#5865F2",
+  },
+  {
+    url: "https://weather.com",
+    background: "#005986",
+  },
+  {
+    url: "https://www.youtube.com",
+    background: "#FF0000",
+  },
+  {
+    url: "https://www.amazon.com",
+    background: "#febd69",
+  },
+  {
+    url: "https://www.twitch.tv",
+    background: "#6441a5",
+  },
+  {
+    url: "https://discord.com",
+    background: "#5865F2",
+  },
+  {
+    url: "https://weather.com",
+    background: "#005986",
   },
 ]
 
@@ -52,38 +92,107 @@ export default function App() {
   }
 
   const Recommended = ()=>{
+    const display = [
+      1,2,1,1,2,1,2
+    ]
+
     const clickRec = (e: React.MouseEvent, url: string)=>{
       let div = e.target as HTMLDivElement
       if(div.className === "rec-button") window.location.replace(url)
     }
 
-    const openSpan = (e: React.MouseEvent<HTMLDivElement>)=>{
-      let div = e.currentTarget as HTMLDivElement
+    const openSpan = (e: React.MouseEvent<HTMLButtonElement>)=>{
+      let div = e.currentTarget as HTMLButtonElement
       let span = div.lastChild as HTMLSpanElement
       span?.classList.toggle("expanded")
     }
 
-    return <div>
-      {urls.map((obj, i)=>{
-        let filteredName = getName(obj.url)
+    let isButton = true
+    const AddButton = (index: number)=>{
+      isButton = false
+      return <div 
+        className='rec-button' 
+        onClick={()=>{setPopUp(true)}} 
+        style={{
+          height: (display[index])*8+"rem",
+        }}
+      >
+        <FontAwesomeIcon icon={faPlus}/>
+      </div>
+    }
+    const Placeholder = (index: number)=>{
+      return <div 
+        className='rec-button' 
+        style={{
+          height: (display[index])*8+"rem",
+        }}
+      >
+      </div>
+    }
 
-        return <div 
+    let jsx: JSX.Element[][] = [
+      [], []
+    ] 
+
+    let loopIterations = Math.floor(urls.length/7) + 1
+    
+    for(let j=0; j<loopIterations;j++) {
+      for(let i=0; i < display.length; i++) {
+        let obj = urls[i]
+        let filteredName: string | undefined = undefined
+        if(obj) filteredName = getName(obj.url)
+  
+        jsx[i < 4 ? 0 : 1].push(obj ? <div 
           className='rec-button' 
-          key={"rec-" + i} 
+          key={"rec-" + i + " " + j} 
           onClick={(e)=>{clickRec(e, obj.url)}} 
-          style={{backgroundColor: obj.background}}
+          data-height={`${display[i]}`}
+          style={{
+            backgroundColor: obj.background,
+            height: (display[i])*8+"rem",
+          }}
         >
-          {filteredName}
-          <div onClick={openSpan}>
-            <FontAwesomeIcon icon={faEllipsisVertical}/>
+          {i}
+          {/* {filteredName} */}
+          <div className='span-container'>
+            <button className='button-open' onClick={openSpan}>
+              <FontAwesomeIcon icon={faEllipsisVertical}/>
+            </button>
             <span className='rec-span'>
               <button onClick={()=>{console.log("edit " + filteredName)}}>Edit</button>
               <button onClick={()=>{console.log("remove " + filteredName)}}>Remove</button>
             </span>
           </div>
+        </div>: isButton ? AddButton(i) : Placeholder(i)
+        )
+      }
+    }
+
+    if(isButton) {
+      jsx[0].push(AddButton(jsx.length))
+
+    }
+
+    // for(let j=index; j<2; j++ ) {
+
+    //   for(let k=urls.length%7;k<7;k++) {
+
+    //     let filteredIndex = k-7*(Math.floor(urls.length/7))
+
+    //     if(k === urls.length%7) jsx[j].push(AddButton(filteredIndex))
+    //     else jsx[j].push(Placeholder(filteredIndex))
+    //   }
+    // }
+
+    return <div className='recomendation-section'>
+      <section>        
+        <div>
+          {jsx[0]}
         </div>
-      })}
-      <button onClick={()=>{setPopUp(true)}}><FontAwesomeIcon icon={faPlus}/></button>
+        <div>
+          {jsx[1]}
+        </div>
+      </section>
     </div>
   }
 
@@ -94,7 +203,7 @@ export default function App() {
     setPopUp(false)
   }
 
-  return <section>
+  return <section className='main'>
     {popUp && <PopUp confirm={addUrl}/>}
     <SearchInput/>
     <Recommended/>
